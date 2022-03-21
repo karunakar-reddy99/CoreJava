55fc829d9610caa7002abe5953b3e790f53a60a4
karunakar-reddy99 <97718902+karunakar-reddy99@users.noreply.github.com>
2022-03-21 16:52:49 +0530

    Commit First

--- 1/8/8.3/Demo.java
+++ 1/8/8.3/Demo.java
@@ -4,7 +4,7 @@ class ThreadA extends Thread{
     public void run( ) {
        for(int i = 1; i <= 5; i++) {
           System.out.println("From Thread A with i = "+ -1*i);
-       }
+       } 
        System.out.println("Exiting from Thread A ...");
     }
 }
