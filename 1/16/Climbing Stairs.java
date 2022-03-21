78c4c935b98c806c96817cb936c9216e14fe3f3f
karunakar-reddy99 <97718902+karunakar-reddy99@users.noreply.github.com>
2022-03-21 18:26:57 +0530

    Commit First

--- 1/16/Climbing Stairs.java
+++ 1/16/Climbing Stairs.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class ClimbingStairs {
 	
 	 static int climbStairs(int N){
-		    if ( N < 2 )
+		    if ( N < 2 ) 
 		        return 1;
 		    else
 		        return climbStairs(N-1) + climbStairs(N-2);
